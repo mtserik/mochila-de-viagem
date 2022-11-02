8b85cd94559b9438f52a597539c53af3bd971181
mtserik <erik.stos.mts@gmail.com>
2022-11-02 11:53:28 -0300

    Finalizando o projeto e excluindo os itens do localStorage, ajustando o codigo para criar os itens a partir do id do ultimo elemento

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@ form.addEventListener("submit", (evento) => {
     if (existe) {
         itemAtual.id = existe.id;
         atualizaElemento(itemAtual);
-        itens[existe.id] = itemAtual;
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
     } else {
-        itemAtual.id = itens.length;
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id +1 : 0
         criaElemento(itemAtual);
         itens.push(itemAtual);
     }
@@ -43,10 +43,29 @@ function criaElemento(objeto) {
 
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += objeto.nome;
+    novoItem.appendChild(botaoDeleta(objeto.id));
 
     lista.appendChild(novoItem);
 }
 
 function atualizaElemento(objeto) {
     document.querySelector("[data-id='"+objeto.id+"']").innerHTML = objeto.quantidade;
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "X";
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id);
+    })
+
+    return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+    tag.remove();
+    itens.splice(itens.findIndex(elemento => elemento.id ===id), 1);
+    localStorage.setItem("itens", JSON.stringify(itens));
+
 }
