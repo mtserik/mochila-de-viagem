16f94400e11d2e7664b4acaa6f0305a2e5f95f97
mtserik <erik.stos.mts@gmail.com>
2022-11-02 11:18:32 -0300

    Verificando se o elemento digitado ja existe e atualizando a visualizacao e armazenamento

--- js/main.js
+++ js/main.js
@@ -11,18 +11,25 @@ form.addEventListener("submit", (evento) => {
 
     const nome = evento.target.elements["nome"];
     const quantidade = evento.target.elements["quantidade"];
+    const existe = itens.find(elemento => elemento.nome === nome.value);
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
+    if (existe) {
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
+        itens[existe.id] = itemAtual;
+    } else {
+        itemAtual.id = itens.length;
+        criaElemento(itemAtual);
+        itens.push(itemAtual);
+    }
 
-    criaElemento(itemAtual);
+    localStorage.setItem("itens", JSON.stringify(itens));
 
     nome.value = "";
     quantidade.value = "";
-
-    itens.push(itemAtual);
-    localStorage.setItem("itens", JSON.stringify(itens));
 })
 
 function criaElemento(objeto) {
@@ -31,10 +38,15 @@ function criaElemento(objeto) {
     novoItem.classList.add("item");
 
     const numeroItem = document.createElement("strong");
+    numeroItem.dataset.id = objeto.id
     numeroItem.innerHTML = objeto.quantidade;
 
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += objeto.nome;
 
     lista.appendChild(novoItem);
+}
+
+function atualizaElemento(objeto) {
+    document.querySelector("[data-id='"+objeto.id+"']").innerHTML = objeto.quantidade;
 }
